01c23477edb1c930447e138760c9ce39832af0a7
danMedina <pokoace95@gmail.com>
2019-12-18 21:43:54 -0600

    primer commit - agregandolo al repositorio

---
+++ datos.php
@@ -0,0 +1,44 @@
+<?php
+    //rcuperando variables del html
+    $nombre = $_POST["nombre"];
+    $correo = $_POST["correo"];
+    $mensaje = $_POST["mensaje"];
+
+    $body = "Nombre: " . $nombre . "<br>Correo: " . $correo ."<br>Mensaje: " . $mensaje; 
+
+    include_once('phpmailer/PHPMailer.php');
+    include_once('phpmailer/SMTP.php');
+
+    $mail = new PHPMailer\PHPMailer\PHPMailer();
+    $mail->IsSMTP(); // enable SMTP
+    $mail->SMTPDebug = 0; // debugging: 1 = errors and messages, 2 = messages only
+    //authentication SMTP enabled
+    $mail->SMTPAuth = true; 
+    $mail->SMTPSecure = 'ssl'; // secure transfer enabled REQUIRED for Gmail
+    $mail->Host = "smtp.gmail.com";
+    //puerto que usa Gmail 465 or 587
+    $mail->Port = 465; 
+    //para acceder a la cuenta
+    $mail->Username = "portafoliojdcm@gmail.com";
+    $mail->Password = "nst.DEXALRAK1995A";
+
+    $mail->SetFrom("portafoliojdcm@gmail.com", $nombre);
+    $mail->AddAddress("ingejosedancm@gmail.com");
+    $mail->Subject = "Te quieren contactar - Portafolio";
+    $msj= $body;
+    $mail->MsgHTML($msj); 
+    $mail->CharSet='UTF-8';
+    
+
+ if(!$mail->Send()) {
+    echo "ERROR: " . $mail->ErrorInfo;
+ } else {
+    echo "<script>
+        alert('Los datos se han enviado correctamente...pronto me pondré en contacto con usted');
+        function redireccionar(){
+            window.location= 'index.html';
+          } 
+          setTimeout ('redireccionar()', 1000); //tiempo expresado en milisegundos
+        </script>";
+ }
+?>
